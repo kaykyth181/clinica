4920dcbc7b35c4a2e6f8c093407ba1632ce62b3b
Kayky Thiago <147095292+kaykyth181@users.noreply.github.com>
2025-04-18 21:12:03 -0300

    update
    atualização pagina principal, whatsapp e navbar.

---
+++ java.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.nav-link').forEach(item => {
+    item.addEventListener('click', function() {
+      // Remove a classe active de todos os links
+      document.querySelectorAll('.nav-link').forEach(link => link.classList.remove('active'));
+      // Adiciona a classe active ao link clicado
+      this.classList.add('active');
+    });
+  });
